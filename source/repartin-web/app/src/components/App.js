9b697d558cd4476e947f5d2d041dd15cb1a5cb63
Almeida <jv.goulart.almeida@hotmail.com>
2018-11-06 09:57:08 -0200

    login facebook and google web

--- source/repartin-web/app/src/components/App.js
+++ source/repartin-web/app/src/components/App.js
@@ -1,13 +1,58 @@
 import React, { Component } from "react";
 import "./App.scss";
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
-export default class App extends Component {
+firebase.initializeApp({
+  apiKey: "AIzaSyAP-W-sqXqyIqK_acOxCGXRbLb-jSy0uGY",
+  authDomain: "repartin-bcc85.firebaseapp.com",
+  projectId: "repartin-bcc85",
+  storageBucket: "repartin-bcc85.appspot.com",
+  messagingSenderId: "816224201193"
+})
+
+class App extends Component {
+  state = { isSignedIn: false }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      console.log("user", user)
+    })
+  }
 
   render() {
-    return ( 
-      <div>
-        <h1>Me ve um x calabresa especial</h1> 
+    return (
+      <div className="App">
+        {this.state.isSignedIn ? (
+          <span>
+            <div>Signed In!</div>
+            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            <img
+              alt="profile picture"
+              src={firebase.auth().currentUser.photoURL}
+            />
+          </span>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
       </div>
     )
   }
 }
+
+export default App
