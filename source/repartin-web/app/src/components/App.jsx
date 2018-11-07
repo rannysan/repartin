ab18897a71dc5dd24bfe26222f371d5807204fcd
renanbatel <renanbatel@gmail.com>
2018-11-07 00:19:05 -0200

    fixing hot replacement firebase bug

--- source/repartin-web/app/src/components/App.jsx
+++ source/repartin-web/app/src/components/App.jsx
@@ -1,25 +1,15 @@
 import React, { Component } from "react";
-import "./App.scss";
-import firebase from "firebase";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import Typography from "@material-ui/core/Typography";
+import "./App.scss";
   
-
-firebase.initializeApp({
-  apiKey: "AIzaSyAP-W-sqXqyIqK_acOxCGXRbLb-jSy0uGY",
-  authDomain: "repartin-bcc85.firebaseapp.com",
-  projectId: "repartin-bcc85",
-  storageBucket: "repartin-bcc85.appspot.com",
-  messagingSenderId: "816224201193"
-})
-
 class App extends Component {
   state = { isSignedIn: false }
   uiConfig = {
     signInFlow: "popup",
     signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+      this.props.firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      this.props.firebase.auth.FacebookAuthProvider.PROVIDER_ID
     ],
     callbacks: {
       signInSuccess: () => false
@@ -27,30 +17,32 @@ class App extends Component {
   }
 
   componentDidMount = () => {
-    firebase.auth().onAuthStateChanged(user => {
+    
+    this.props.firebase.auth().onAuthStateChanged(user => {
       this.setState({ isSignedIn: !!user })
-      console.log("user", user)
     })
   }
 
   render() {
     return (
       <div className="App">
-        <Typography variant="h4" component="h4">aa</Typography>
+      
+        <Typography variant="h4" component="h4">Login</Typography>
+
         {this.state.isSignedIn ? (
           <span>
             <div>Signed In!</div>
-            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
-            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            <button onClick={() => this.props.firebase.auth().signOut()}>Sign out!</button>
+            <h1>Welcome {this.props.firebase.auth().currentUser.displayName}</h1>
             <img
               alt="profile picture"
-              src={firebase.auth().currentUser.photoURL}
+              src={this.props.firebase.auth().currentUser.photoURL}
             />
           </span>
         ) : (
           <StyledFirebaseAuth
             uiConfig={this.uiConfig}
-            firebaseAuth={firebase.auth()}
+            firebaseAuth={this.props.firebase.auth()}
           />
         )}
       </div>
--- source/repartin-web/app/src/index.jsx
+++ source/repartin-web/app/src/index.jsx
@@ -1,9 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/auth";
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
 import "typeface-roboto";
 
-render( <App />, document.getElementById( "app" ) );
+try {
+  firebase.initializeApp( {
+    apiKey: "AIzaSyAP-W-sqXqyIqK_acOxCGXRbLb-jSy0uGY",
+    authDomain: "repartin-bcc85.firebaseapp.com",
+    projectId: "repartin-bcc85",
+    storageBucket: "repartin-bcc85.appspot.com",
+    messagingSenderId: "816224201193"
+  } );
+} catch( error ) {
+  if( !/already exists/.test( error.message ) ) {
+    console.error( error.stack );
+  }
+}
+
+render( <App firebase={ firebase }/>, document.getElementById( "app" ) );
 
 // For Webpack Hot Module Replacement
-!module.hot || module.hot.accept();
+!module.hot || module.hot.accept( App );
