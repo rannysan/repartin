a3bb26649601bf4610231159f765309bb40ee0e2
renanbatel <renanbatel@gmail.com>
2018-11-10 22:46:46 -0200

    testing services module

--- source/repartin-web/app/src/components/App/index.jsx
+++ source/repartin-web/app/src/components/App/index.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import View from "./View";
+import { getUserById } from "services-commons";
 
 export default class extends Component {
   state = {
     isSignedIn: false
   };
+
+  async componentWillMount() {
+    const teste = await getUserById( "teste" );
+    console.log( teste );
+  }
 
   componentDidMount = () => {
 
