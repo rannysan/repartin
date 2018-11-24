ade6966fb9c74a3f78b1683ae8b527a838338221
Gabriel Romero <gabrielromerods@hotmail.com>
2018-11-24 20:02:00 -0200

    Controller palette

---
+++ source/node-api/src/Controller/PaletteController.js
@@ -0,0 +1,63 @@
+var model = require('../Schemas/Palette.js');
+
+module.exports = {
+    create: function (req, res) {
+        var palette = new model({
+            primary: req.body.primary,
+            onPrimary: req.body.onPrimary,
+            secondary: req.body.secondary,
+            onSecondary: req.body.onSecondary
+        });
+
+        palette.save(function (err, palette) {
+            if (err) { return res.status(500).json({ message: 'Ops! Ocorreu um erro ao salvar nova paleta de cores', error: err }) };
+            return res.json({ palette: palette, message: 'paleta de cores criada com sucesso!' });
+        });
+    },
+
+    getByName: function (req, res) {
+        var name = req.body.name;
+        model.findOne({ name: name }, function (err, palette) {
+            if (err) { return res.status(500).json({ message: 'Ops! Ocorreu um erro ao buscar paleta de cores', error: err }) };
+            if (palette) {
+                return res.json({ palette: palette, message: 'paleta de cores encontrada!' });
+            } else {
+                return res.json({ palette: palette, message: 'paleta de cores não encontrada :(!' });
+            }
+        });
+    },
+
+    getById: function (req, res) {
+        var id = req.body.id;
+        model.findOne({ _id: id }, function (err, palette) {
+            if (err) { return res.status(500).json({ message: 'Ops! Ocorreu um erro ao buscar paleta de cores', error: err }) };
+            if (palette) {
+                return res.json({ palette: palette, message: 'paleta de cores encontrada!' });
+            } else {
+                return res.json({ palette: palette, message: 'paleta de cores não encontrada :(!' });
+            }
+        });
+    },
+
+    deleteById: function (req, res) {
+        var id = req.body.id;
+        model.findByIdAndRemove(id, function (err, palette) {
+            if (err) { return res.status(500).json({ message: 'Ops! Ocorreu um erro ao deletar paleta de cores', error: err }) };
+            return res.json({ message: 'paleta de cores excluida com sucesso!' });
+        });
+    },
+
+    updateById: function (req, res) {
+        var id = req.body.id;
+        var palette = { 
+            primary: req.body.primary,
+            onPrimary: req.body.onPrimary,
+            secondary: req.body.secondary,
+            onSecondary: req.body.onSecondary
+        };
+        model.findByIdAndUpdate(id, palette, function (err, palette) {
+            if (err) { return res.status(500).json({ message: 'Ops! Ocorreu um erro ao deletar paleta de cores', error: err }) };
+            return res.json({ message: 'paleta de cores atualizada com sucesso!' });
+        });
+    }
+};
--- source/node-api/src/Schemas/Palette.js
+++ source/node-api/src/Schemas/Palette.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose')
 
-// Create palette schema 
 const paletteSchema = mongoose.Schema({
     primary: String,
     onPrimary: String,
     secondary: String,
     onSecondary: String
 });
-var Palette = mongoose.model('palettes', paletteSchema);
 
-module.exports.Palette = Palette;
+module.exports = mongoose.model('palettes', paletteSchema);
