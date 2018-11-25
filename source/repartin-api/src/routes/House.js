e1d329a2bcd75f8c93c25058e3cc14a5ac50e671
Mateus Merscher <mateusmerscher@hotmail.com>
2018-11-25 21:22:53 -0200

    Get houseAdmin, and get houseMembers add to controllers and routes

--- source/repartin-api/src/routes/House.js
+++ source/repartin-api/src/routes/House.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controller/HouseController.js');
+let user = require('../controller/UserController.js');
 
 
 router.post('/house', controller.create);
 router.get('/house/:id', controller.getById);
 router.get('/house', controller.getById);
-router.put('/house/:id', controller.updateById)
+router.put('/house/:id', controller.updateById);
+router.get('/house/members/:house', user.getByHouse);
+router.get('/house/admin/:id', controller.getAdmin);
+
 
 module.exports = router;
