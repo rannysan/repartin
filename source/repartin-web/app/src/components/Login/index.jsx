4c876afaa1144fe58413d42ad921c141510f7810
João Goulart <jv.goulart.almeida@hotmail.com>
2018-11-24 23:48:32 -0200

    service generic

--- source/repartin-web/app/src/components/Login/index.jsx
+++ source/repartin-web/app/src/components/Login/index.jsx
@@ -1,21 +1,37 @@
 import React, { Component } from "react";
 import View from "./View";
-import service from "../../services/userService";
+import service from "../../services/service";
 
 export default class extends Component {
 
    componentDidMount = () => {
     this.props.firebaseAuth().onAuthStateChanged(async auth => {
       if (auth) {
-        const user = await service.getUserById(auth.uid);
+        const user = await service.getById('user',auth.uid);
         if (user) {
-          await service.updateUser(auth);
-          //redirect
+          if (!user.removed) {
+            await service.update('user', {
+              name: auth.displayName,
+              email: auth.email,
+              uid: auth.uid,
+              houseID: null,
+              removed: false
+            });
+            //redirect
+          }
+
         } else {
-          await service.createUser(auth);
+          await service.create('user', { 
+            name: auth.displayName,
+            email: auth.email,
+            uid: auth.uid,
+            houseID: null,
+            removed: false
+          });
           //redirect
         }
       }
+
     });
   };
   
---
+++ source/repartin-web/app/src/services/service.jsx
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+const url = 'http://localhost:3000';
+
+
+const service = {
+    getById: async (path, id) => {
+        console.log(`Buscando documento em ${path} usando o id: ${id}`);
+
+        return axios.get(`${url}/${path}/${id}`)
+          .then((response) =>  {
+            if (response.status == 200) {
+                return response.data;
+            } else {
+                return undefined;
+            }
+          })
+          .catch((error) =>  {
+            console.log(`Erro ao buscar documento em ${path}. Seguinte erro foi disparado: ${JSON.stringify(error)}`);
+            return undefined;
+          });
+    },
+
+    create: async (path, body) => {
+        console.log(`Criando novo documento em ${path} com o body: ${JSON.stringify(body)}`);
+
+        return axios.post(`${url}/${path}/`, body)
+            .then(response => {
+                return response.data;
+            }).catch((error) =>  {
+                console.log(`Erro ao inserir documento em ${path}. Seguinte erro foi disparado: ${JSON.stringify(error)}`);
+                return undefined;
+            });
+    },
+
+    update: async (path, body) => {
+        console.log(`Atualizando documento em ${path} com o body: ${JSON.stringify(body)}`);
+
+        return axios.put(`${url}/${path}/`, body)
+            .then(response => {
+                return response.data;
+            }).catch((error) =>  {
+                console.log(`Erro ao atualizar documento em ${path}. Seguinte erro foi disparado: ${JSON.stringify(error)}`);
+                return undefined;
+            });
+    }
+}
+
+export default service;
