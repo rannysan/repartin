1cc8848af78cbf643256c5aa7d2d1ede983da090
João Goulart <jv.goulart.almeida@hotmail.com>
2018-11-25 17:12:04 -0200

    ajuste service

--- source/repartin-web/app/src/components/Login/index.jsx
+++ source/repartin-web/app/src/components/Login/index.jsx
@@ -2,16 +2,20 @@ import React, { Component } from "react";
 import View from "./View";
 import service from "../../services/service";
 import { firebaseConnect  } from 'react-redux-firebase'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom';
 
 class Login extends Component {
 
    componentDidMount = () => {
     this.props.firebase.auth().onAuthStateChanged(async auth => {
+
       if (auth) {
-        const user = await service.getById('user',auth.uid);
+        const {user, message} = await service.getById('user',auth.uid);
         if (user) {
           if (!user.removed) {
-            await service.update('user', {
+            await service.update('user', user._id, {
               name: auth.displayName,
               email: auth.email,
               uid: auth.uid,
@@ -20,7 +24,6 @@ class Login extends Component {
             });
             //redirect
           }
-
         } else {
           await service.create('user', { 
             name: auth.displayName,
@@ -44,4 +47,9 @@ class Login extends Component {
   }
 }
 
-export default firebaseConnect()(Login);
+
+
+export default compose(
+  firebaseConnect(),
+  withRouter,
+)(Login);
