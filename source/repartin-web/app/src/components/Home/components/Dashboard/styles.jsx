6e7a2c3356ffb65bd0b8b212725eda04f6d7a73d
Breno Gregório Angelotti <breno@forlogic.net>
2018-11-26 23:39:57 -0200

    correção componentes layout dashboard

--- source/repartin-web/app/src/components/Home/components/Dashboard/styles.jsx
+++ source/repartin-web/app/src/components/Home/components/Dashboard/styles.jsx
@@ -3,6 +3,10 @@ import { createStyles } from "@material-ui/core/";
 export default ( theme ) => {
 
   return createStyles( {
-
+    grid:{
+      padding: "16px", 
+      maxWidth: "800px", 
+      margin: "auto"
+    }
   } );
 }
